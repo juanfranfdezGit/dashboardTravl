9e966656f725426883b83b6c1d61700a672690a2
juanfranfdezGit <jfranromfdez@gmail.com>
2025-05-08 09:15:17 +0200

    feat: button styles

--- src/components/navbar/ThemeSelector.jsx
+++ src/components/navbar/ThemeSelector.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { ThemeContext } from "../../context/themeContext";
 import { useContext } from "react";
 
+import { MdOutlineWbSunny } from "react-icons/md";
+import { FiMoon } from "react-icons/fi";
+
 export default function ThemeSelector() {
 
     const { theme, toggleTheme } = useContext(ThemeContext);
@@ -10,7 +13,7 @@ export default function ThemeSelector() {
     return (
         <StyledThemeSelector>
             <button onClick={toggleTheme}>
-                {theme === 'light' ? '🌞' : '🌙'}
+                {theme === 'light' ? <MdOutlineWbSunny className='ico' /> : <FiMoon className='ico' />}
             </button>
         </StyledThemeSelector>
     )
@@ -22,8 +25,13 @@ const StyledThemeSelector = styled.div `
     button {
         width: 40px;
         height: 40px;
-        background: var(--black);
+        background: var(--pending);
         border: none;
+        border-radius: 8px;
         cursor: pointer;
+
+        .ico {
+            font-size: 24px;
+        }
     }
 `; 
